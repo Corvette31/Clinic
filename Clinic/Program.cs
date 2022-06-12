725665cfa7d1392e313d1894ca3ed79d6feac8de
Corvette31 <corvette.ss@yandex.ru>
2022-06-12 22:40:16 +0300

    Добавьте файлы проекта.

---
+++ Clinic/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Clinic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int peopleInLine;
+            int receptionTime = 10;
+            int waitingHours;
+            int waitingMinutes;
+
+            Console.Write("Сколько человек в очереди впереди вас? :");
+            peopleInLine = Convert.ToInt32(Console.ReadLine());
+            waitingHours = (peopleInLine * receptionTime) / 60;
+            waitingMinutes = (peopleInLine * receptionTime) % 60;
+            Console.Write($"Вы должны отстоять в очереди {waitingHours} часа и {waitingMinutes} минут.");
+        }
+    }
+}
